75b44dd1bd80419d43177227ae731ddb145d8c1c
kyledunne <kyledunne@berkeley.edu>
2020-02-07 20:20:59 -0500

    Restart from navigation drawer template

---
+++ app/src/main/java/me/kyledunne/kylesmusicreviewsapp/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package me.kyledunne.kylesmusicreviewsapp.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
